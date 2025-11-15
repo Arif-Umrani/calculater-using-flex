65669a4b57e2dbe2e86c4132a08b96d7cc725d4c
Arif Ali <meer.aafay@gmail.com>
2025-11-15 06:47:22 -0800

    add gpt code page

---
+++ gpt.js
@@ -0,0 +1,53 @@
+<script>
+
+let value1 = "";
+let value2 = "";
+let operator = "";
+
+function press(num) {
+    document.getElementById("screen").innerHTML += num;
+}
+
+function clearScreen() {
+    document.getElementById("screen").innerHTML = "";
+    value1 = "";
+    value2 = "";
+    operator = "";
+}
+
+function setOp(op) {
+    value1 = document.getElementById("screen").innerHTML;
+    operator = op;
+    document.getElementById("screen").innerHTML = "";
+}
+
+function calculate() {
+    value2 = document.getElementById("screen").innerHTML;
+
+    let result;
+
+    switch(operator) {
+        case "+":
+            result = Number(value1) + Number(value2);
+            break;
+
+        case "-":
+            result = Number(value1) - Number(value2);
+            break;
+
+        case "*":
+            result = Number(value1) * Number(value2);
+            break;
+
+        case "/":
+            result = Number(value1) / Number(value2);
+            break;
+
+        default:
+            result = "Error!";
+    }
+
+    document.getElementById("screen").innerHTML = result;
+}
+
+</script>
