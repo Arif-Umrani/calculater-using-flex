9164a1d218a4eada455704fe5561efe6ed152106
Waqar Ali <waqardotes@gmail.com>
2025-11-15 19:51:19 +0500

    add buttons setup

---
+++ index.js
@@ -0,0 +1,19 @@
+//  screen setup 
+let screenValue = '';
+let opt;
+let firstNum; 
+
+document.getElementById("screen").value = screenValue;
+
+
+function press(key) {
+    
+}
+
+function setOperator(key) {
+    opt = key;
+
+    console.log('opt', opt)
+}
+
+
